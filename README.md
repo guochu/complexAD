18588f53c17646d23cce403010c3cfac9750aa97
guochu <guochu604b@gmail.com>
2020-03-02 09:41:33 +0800

    add commin functions

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Examples of automatic differentiation mentioned in the paper arXiv:~. The fold src contains utility functions
 which are used to derive the numerical gradient by finite difference method.
 
-* **In the file "common_funcs.jl", we show the definition of the adjoint function for the non-holomorphic function "dot", and checked the gradient.
+* In the file "common_funcs.jl", we show the definition of the adjoint function for the non-holomorphic function "dot", and checked the gradient.
 
-* **In the file "arjovsky16.jl", we parameterize the W matrix using a mixture of real and complex numbers, which is used for unitary recurrent neural network in the paper [Unitary Evolution Recurrent Neural Networks](http://proceedings.mlr.press/v48/arjovsky16.pdf)
+* In the file "arjovsky16.jl", we parameterize the W matrix using a mixture of real and complex numbers, which is used for unitary recurrent neural network in the paper [Unitary Evolution Recurrent Neural Networks](http://proceedings.mlr.press/v48/arjovsky16.pdf)
 
-* **In the file "wisdom.jl", we use complex numbers to parameterize the W matrix propose in the paper [Full-Capacity Unitary Recurrent Neural Networks](http://papers.nips.cc/paper/6327-full-capacity-unitary-recurrent-neural-networks). Compared to the original work which split the W matrix into two matrices which contain the real and imaginary parts separately, we use a single complex matrix to represent it, and the gradient of any complex function built with W can be computed as straightforward as the real case.
+* In the file "wisdom.jl", we use complex numbers to parameterize the W matrix propose in the paper [Full-Capacity Unitary Recurrent Neural Networks](http://papers.nips.cc/paper/6327-full-capacity-unitary-recurrent-neural-networks). Compared to the original work which split the W matrix into two matrices which contain the real and imaginary parts separately, we use a single complex matrix to represent it, and the gradient of any complex function built with W can be computed as straightforward as the real case.
 
 
--- comm_funcs.jl
+++ comm_funcs.jl
@@ -3,22 +3,50 @@ push!(LOAD_PATH, "src")
 
 using Zygote
 using Zygote: @adjoint
-
+using FFTW
 using LinearAlgebra: dot
 using Util
 
-# # correct gradient for dot
-# @adjoint dot(a::AbstractArray, b::AbstractArray) = dot(a, b), z -> (conj(z) .* b, z .* a)
+# Examples of adjoint functions from some common complex functions
+# For most of the following functions, Zygote default adjoint functions 
+# are correct, but for non-holomorphic ones, such as dot, Zygote may return incorrect 
+# gradients currectly.
+@adjoint sin(z::Number) = sin(z), v -> (v * cos(conj(z)),)
+@adjoint exp(z::Number) = exp(z), v -> (v * exp(conj(z)),)
+@adjoint log(z::Number) = log(z), v -> (v / conj(z),)
+@adjoint +(z::Number, w::Number) = z + w, v -> (v, v)
+@adjoint *(z::Number, w::Number) = z * w, v -> (v*conj(w), v*conj(z))
+@adjoint /(z::Number, w::Number) = z / w, v -> (v/conj(w), -v*conj(z)/conj(w)^2)
+@adjoint *(z::Number, w::AbstractVector) = z * w, v -> (dot(w, v), v .* conj(z))
+@adjoint read(z::Number) = real(z), v -> (real(v),)
+@adjoint imag(z::Number) = imag(z), v -> (im*real(v),)
+@adjoint abs(z::Number) = abs(z), v -> (real(v)*z/abs(z),)
+@adjoint dot(z::AbstractArray, w::AbstractArray) = dot(z, w), v -> (conj(v) .* w, v .* z)
+outer(z::AbstractVector, w::AbstractVector) = reshape(kron(w, z), length(z), length(w))
+@adjoint outer(z::AbstractVector, w::AbstractVector) = outer(z, w), v -> (v*conj(w), z'*v)
+@adjoint *(z::AbstractMatrix, w::AbstractMatrix) = z*w, v -> (v * w', z' * v)
+@adjoint fft(z::AbstractVector) = fft(z), v -> (ifft(v) .* length(z),)
+@adjoint ifft(z::AbstractVector) = ifft(z), v -> (fft(v) ./ length(z),)
+
 
 random_hermitian(L::Int) = begin
     m = randn(Complex{Float64}, L, L)
     return m + m'
 end
 
-m = random_hermitian(5)
+function check_dot()
+	m = random_hermitian(5)
+	loss(x::AbstractArray) = real(dot(x, m * x) / dot(x, x))
+	return check_gradient(loss, randn(Complex{Float64}, 5))
+end
 
-loss(x::AbstractArray) = real(dot(x, m * x) / dot(x, x))
+function check_outer()
+	m = randn(Complex{Float64}, 5)
+	loss(x::AbstractVector) = real(sum(outer(x, m)))
+	return check_gradient(loss, randn(Complex{Float64}, 5))
+end
 
-x = randn(Complex{Float64}, 5)
 
-println(check_gradient(loss, x, verbose=1))
+println(check_dot())
+println(check_outer())
+
